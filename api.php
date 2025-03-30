832fe7f4f592d00619cd298540ce6d5c67b4dcd7
K4mD4m <KamyszekDamian@outlook.com>
2025-03-30 12:30:49 +0200

    PUT added

--- api.php
+++ api.php
@@ -3,7 +3,7 @@
 //Ustawienie nagłówków, aby API zwracało dane w formacie JSON i obsługiwało CORS
 header("Content-Type: application/json;");
 header("Access-Control-Allow-Origin: *"); //Zezwolenie na dostęp z dowolnej domeny
-header("Access-Control-Allow-Methods: GET, POST, DELETE");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT");
 
 // Pliki do przechowywania danych (symulacja bazy danych)
 $dataFile = "data.json";
@@ -68,7 +68,34 @@ if ($method === 'GET') {
     file_put_contents($dataFile, json_encode($users, JSON_PRETTY_PRINT));
 
     echo json_encode(["success" => "Użytkownik usunięty", "id" => $id]);
+} elseif ($method === 'PUT') {
+    //Odczytujemy dane wysłane w PUT
+    $input = json_decode(file_get_contents("php://input"), true);
+
+    //Sprawdzamy, czy przesłano poprawne dane 
+    $id = $input['id'] ?? null;
+    $newName = $input['name'] ?? null;
+
+    if (!$id || !$newName) {
+        echo json_encode(["error" => "Brak ID lub nazwy użytkownika"]);
+        exit;
+    }
+
+    //Szukamy użytkownika o podanym ID do aktualizacji
+    $index = array_search($id, array_column($users, 'id'));
+    if ($index === false) {
+        echo json_encode(["error" => "Użytkownik nie istnieje"]);
+        exit;
+    }
+
+    // Zaktualizowanie danych użytkownika
+    $users[$index]['name'] = htmlspecialchars($newName);
+
+    // Zapisujemy zaktualizowaną tablicę do pliku JSON
+    file_put_contents($dataFile, json_encode($users, JSON_PRETTY_PRINT));
+
+    // Zwracamy odpowiedź z zaktualizowanym użytkownikiem
+    echo json_encode(["success" => "Użytkownik zaktualizowany", "user" => $users[$index]]);
 } else {
-    // Obsługa innych metod
-    echo json_encode(["error" => "Nieobsługiwana metoda"]);
+    echo json_encode(["error" => "Nieobsługiwana metoda HTTP"]);
 }
